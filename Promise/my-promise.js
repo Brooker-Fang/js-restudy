7e3e4b2ce74207c71a0acaebbf045f6fa92607cd
comedyOfKing <f_hhong@163.com>
2021-05-01 11:47:46 +0800

    note

--- Promise/my-promise.js
+++ Promise/my-promise.js
@@ -27,8 +27,8 @@ class MyPromise {
   status = PENDING
   value = undefined
   error = undefined
-  successCallback = undefined
-  failCallback = undefined
+  successCallback = [] // then方法多次调用，把回调函数存储到数组
+  failCallback = []
   resolve = (val) => {
     if (this.status !== PENDING) return false;
     // 将状态更改为成功
@@ -37,7 +37,10 @@ class MyPromise {
     this.value = val
     // 处理异步逻辑
     // 判断成功回调是否存在，存在则调用
-    this.successCallback && this.successCallback(this.value)
+    // this.successCallback && this.successCallback(this.value)
+    while(this.successCallback.length) {
+      this.successCallback.shift()(this.value)
+    }
   }
   reject = (error) => {
     if (this.status !== PENDING) return false;
@@ -47,30 +50,77 @@ class MyPromise {
     this.error = error
     // 处理异步逻辑
     // 判断成功回调是否存在，存在则调用
-    this.failCallback && this.failCallback(this.error)
+    // this.failCallback && this.failCallback(this.error)
+    while(this.failCallback.length) {
+      this.failCallback.shift()(this.error)
+    }
   }
   then = (successCallback, failCallback) => {
-    if(this.status === FULFILLED) {
-      successCallback(this.value)
-    } else if(this.status === REJECTED) {
-      failCallback(this.error)
-    } else {
-      // 当前状态是等待状态
-      // 保存成功回调 和 失败回调
-      this.successCallback = successCallback
-      this.failCallback = failCallback
-    }
+    // 创建新的promise并返回，用于支持链式调用
+    let p2 = new MyPromise((resolve, reject) => {
+      // 立即执行
+      if(this.status === FULFILLED) {
+        let res = successCallback(this.value)
+        // resolve(res)
+        /* 
+         1、判断res的值是普通值还是promise对象
+         2、如果是普通值  直接调用resolve
+         3、如果是promise对象 查看promise对象返回的结果
+         4、再跟进promise对象返回的结果  决定调用resolve 还是调用reject
+        */
+       resolvePromise(res, resolve, reject)
+      } else if(this.status === REJECTED) {
+        failCallback(this.error)
+      } else {
+        // 当前状态是等待状态
+        // 保存成功回调 和 失败回调
+        this.successCallback.push(successCallback)
+        this.failCallback.push(failCallback)
+      }
+    })
+    
+    return p2 // 
+  }
+}
+function resolvePromise(res, resolve, reject) {
+  if (res instanceof MyPromise) {
+    // promise对象
+    console.log('is promise')
+    res.then(val => resolve(val), (error) => reject(error))
+  } else {
+    // 普通值
+    resolve(res)
   }
 }
 let p = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('成功')
-  },2000)
-  // resolve('成功')
+  // 异步
+  // setTimeout(() => {
+  //   resolve('成功')
+  // },2000)
+  resolve('成功')
   // reject('失败')
+})
+let p2 = new MyPromise((resolve, reject) => {
+  resolve('p2 resolve')
 })
 p.then((val) => {
   console.log(val)
+  // 返回普通值
+  return p2
+}, (error) => {
+  console.log(error)
+}).then((v) => {
+  console.log(v)
+  // 返回promise
+  return p2
+})
+p.then(v => {
+  console.log(v)
+}, (error) => {
+  console.log(error)
+})
+p.then(v => {
+  console.log(v)
 }, (error) => {
   console.log(error)
 })
